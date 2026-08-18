4655d1d954821a92be08572e93a99e6c792aee0b
MubinaMubina <159083360+MubinaMubina@users.noreply.github.com>
2026-08-18 22:59:47 +0500

    added new lines of code

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,39 +6,6 @@
 int main(int argc, char **argv)
 {
     int i = 1;
-    int j;
-
-    if(argc >= 2)
-    {
-        std::string result;
-        while(argv[i][j])
-        {   
-            j = 0;
-            if(argv[i][j] == ' ')
-                j++;
-            result[] = argv[i][j];
-            j++;
-        }
-        std::cout << " ";
-        i++;
-
-    }
-    else
-    {
-        
-    }
-    std::cout << "\n";
-    return 0;
-}
-
-
-#include <iostream>
-#include <string>
-#include <cctype>
-
-int main(int argc, char **argv)
-{
-    int i = 1;
     if (argc < 2)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
@@ -52,9 +19,11 @@ int main(int argc, char **argv)
         int j = 0;
         while (argv[i][j])
         {
-            static_cast<char>(std::toupper(static_cast<unsigned char>(argv[i][j])));
-            result += argv[i][j];
-            j++; 
+            
+            
+            char upper_c = static_cast<char>(std::toupper(static_cast<unsigned char>(argv[i][j])));
+            result += upper_c;
+            j++;
         }
         i++;
     }
